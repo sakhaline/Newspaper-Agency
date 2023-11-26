4770ea7ae1bef9c7a04a7a3c25e058190f6e0fd4
sakhaline <alexandra.ivva@gmail.com>
2023-11-26 20:23:45 +0200

    Add newspaper-agency-lmq5.onrender.com to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "newspaper-agency-lmq5.onrender.com"]
 
 
 # Application definition
