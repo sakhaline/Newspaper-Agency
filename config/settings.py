96643e043eb0e49e4e3dcd1249d61aa6998c1c07
sakhaline <alexandra.ivva@gmail.com>
2023-11-26 18:03:46 +0200

    Update settings.py & requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ed46hen38=qh%kp8ktgk(4y1_p8t+lanf2#7c86qxsame6=@f0"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-ed46hen38=qh%kp8ktgk(4y1_p8t+lanf2#7c86qxsame6=@f0")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
