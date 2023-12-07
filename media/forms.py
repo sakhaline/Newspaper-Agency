c969807333ad149d633f6647fb9cdcdb3073853f
sakhaline <alexandra.ivva@gmail.com>
2023-12-07 07:38:51 +0200

    Fix search form & filter form

--- media/forms.py
+++ media/forms.py
@@ -15,12 +15,29 @@ class NewspaperCreationForm(forms.ModelForm):
         fields = "__all__"
 
 
+# class NewspaperFilterForm(forms.Form):
+#     topic_name = forms.ModelChoiceField(
+#         queryset=Topic.objects.all(),
+#         empty_label="All topics",
+#         required=False,
+#     )
+#     query_search = forms.CharField(
+#         max_length=255,
+#         required=False,
+#         label="",
+#         widget=forms.TextInput(attrs={"placeholder": "Search..."})
+#     )
+
+
 class NewspaperFilterForm(forms.Form):
     topic_name = forms.ModelChoiceField(
         queryset=Topic.objects.all(),
         empty_label="All topics",
         required=False,
     )
+
+
+class NewspaperSearchForm(forms.Form):
     query_search = forms.CharField(
         max_length=255,
         required=False,
